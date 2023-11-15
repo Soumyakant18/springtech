4cca5c245a0dc29555933e8a491485fc03efc289
soumyakanta rout <soumykantarout@gmail.com>
2023-11-15 19:07:08 +0530

    first commit

---
+++ src/test/java/com/example/springtech/SpringtechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
